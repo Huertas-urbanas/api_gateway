038939e429944c1388475e909315b1ec6c8b1535
ManQui25 <90158740+ManQui25@users.noreply.github.com>
2021-12-09 19:36:34 -0500

    Delete Api Gateway directory

--- Api Gateway/src/server.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-        auth_api_url: 'https://auth-ms-backend.herokuapp.com',
-        comment_api_url: 'https://comment-ms-be.herokuapp.com',
-        post_api_url: 'https://post-ms-be.herokuapp.com',
-        recommendations_api_url: 'https://recommendations-ms.herokuapp.com',
-};
--- Api Gateway/src/typeDefs/index.js
+++
@@ -1,8 +0,0 @@
-const authTypeDefs = require('./auth_type_defs');
-const commentTypeDefs = require('./comment_type_defs');
-const postTypeDefs = require('./post_type_defs')
-const recommendationTypeDefs = require('./recommendation_type_defs');
-
-const schemasArrays = [authTypeDefs, commentTypeDefs, postTypeDefs, recommendationTypeDefs];
-
-module.exports = schemasArrays;
